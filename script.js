4ba57cdb40a36a4775c171fd2a56b3c21810a40b
Courtney <courtneymkirk@hotmail.com>
2022-01-08 11:11:11 -0500

    fix syntax error

--- script.js
+++ script.js
@@ -4,15 +4,10 @@ $("#currentDay").text(moment().format("MMM Do YYY, h:mm:ss a"));
 $(".saveBtn").on("click", function () {
 
   console.log(this);
-  var text = $(this)child(".description").valueOf();
+  var text = $(this).child(".description").valueOf();
   var time = $(this).parent().attr("id");
 
   //items in local storage
   localStorage.setItem(time, text);
-}
-
-
-
-
-
 })
+});
