c60aa311761e3c7eb81bfa78a543268a17cba071
Courtney <courtneymkirk@hotmail.com>
2022-01-08 11:20:55 -0500

    added hours and checking local time

--- script.js
+++ script.js
@@ -10,4 +10,43 @@ $(".saveBtn").on("click", function () {
   //items in local storage
   localStorage.setItem(time, text);
 })
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  function hourTracker() {
+    var currentHour = moment().hour();
+
+    $(".time=block").each(function (){
+      var blockHour = parseInt($(this).attr("id").split("hour")[i]);
+      console.log(blockHour, currentHour)
+
+      if (blockHour < currentHour){
+        $(this).addClass("past");
+        $(this).addClass("present");
+        $(this).addClass("future");
+
+
+      }
+
+    }
+
+
+
+
+  }
+
+
+
+
+
+
 });
